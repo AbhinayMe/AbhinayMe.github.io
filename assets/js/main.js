1c05d8c996a0e0e9906402e2768be48cfe96092a
Abhinay <i@abhinay.me>
2018-08-02 13:54:01 +0800

    Change theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Radius by TEMPLATED
+	Snapshot by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
@@ -7,19 +7,17 @@
 (function($) {
 
 	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
 	});
 
 	$(function() {
 
 		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$footer = $('#footer');
+			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -41,37 +39,63 @@
 				);
 			});
 
-		// Header.
-			$header.each( function() {
+		// Scrolly.
+			$('.scrolly').scrolly();
 
-				var t 		= jQuery(this),
-					button 	= t.find('.button');
+		// Gallery.
+			$('.gallery').each(function() {
 
-				button.click(function(e) {
+				var	$gallery = $(this),
+					$content = $gallery.find('.content');
 
-					t.toggleClass('hide');
+				// Poptrox.
+					$content.poptrox({
+						usePopupCaption: true
+					});
 
-					if ( t.hasClass('preview') ) {
-						return true;
-					} else {
-						e.preventDefault();
-					}
+				// Tabs.
+					$gallery.each( function() {
 
-				});
+						var $this = $(this),
+							$tabs = $this.find('.tabs a'),
+							$media = $this.find('.media');
 
-			});
+						$tabs.on('click', function(e) {
 
-		// Footer.
-			$footer.each( function() {
+							var $this = $(this),
+								tag = $this.data('tag');
 
-				var t 		= jQuery(this),
-					inner 	= t.find('.inner'),
-					button 	= t.find('.info');
+							// Prevent default.
+							 	e.preventDefault();
 
-				button.click(function(e) {
-					t.toggleClass('show');
-					e.preventDefault();
-				});
+							// Remove active class from all tabs.
+								$tabs.removeClass('active');
+
+							// Reapply active class to current tab.
+								$this.addClass('active');
+
+							// Hide media that do not have the same class as the clicked tab.
+								$media
+									.fadeOut('fast')
+									.each(function() {
+
+										var $this = $(this);
+
+										if ($this.hasClass(tag))
+											$this
+												.fadeOut('fast')
+												.delay(200)
+												.queue(function(next) {
+													$this.fadeIn();
+													next();
+												});
+
+									});
+
+						});
+
+					});
+
 
 			});
 
